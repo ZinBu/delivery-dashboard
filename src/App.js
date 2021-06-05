747fda50ca9b65bf449d328d8826562716553877
drs <romadmt92@gmail.com>
2021-06-05 16:22:34 +0300

    d

--- src/App.js
+++ src/App.js
@@ -4,21 +4,24 @@ import {Header} from './components/Static'
 import {HubForm} from './components/Forms'
 
 
-const InfoLabel = (props) => <p style={{"color": "blue"}}>{props.text}</p>
+const InfoLabel = (props) => {
+  const visibility = {"visibility": props.visible ? "visible" : "hidden"}
+  return <div id="info-label" className="info-label fadeIn" style={visibility}><p className="info-label-p">{props.text}</p></div>
+}
 
 
 class App extends Component {
   state = {
       hubData: [],
-      labelText: null
+      labelText: null,
+      labelVisible: false,
   }
 
   setHubData = (hubData) => {
     this.setState({hubData: hubData})
     if (!hubData || hubData.length < 1) {
-      this.setState({labelText: "Нет данных!"})
-    } else {
-      this.setState({labelText: null})
+      this.setState({labelText: "Нет данных!", labelVisible: true})
+      setTimeout(() => this.setState({labelText: null, labelVisible: false}), 2000)
     }
   }
 
@@ -26,7 +29,7 @@ class App extends Component {
       return (
         <div className="container">
           <Header text="Дашборд" />
-          <InfoLabel text={this.state.labelText}/>
+          <InfoLabel visible={this.state.labelVisible}  text={this.state.labelText}/>
           <HubForm setAppHubData={this.setHubData} />
           <Table hubData={this.state.hubData} />
         </div>
